18a59ac0416e2913c7335d24594c58d9c11aaa16
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2018-09-12 16:52:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 function happyHolidays() {
   return `Happy holidays!`
 }
-function happyHolidaysTo() {
+function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
-function happyHolidayTo() {
+function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
-function holidayCountdown() {
+function holidayCountdown(days, holiday) {
   return `It's ${days} days until ${holiday}!`
 }
